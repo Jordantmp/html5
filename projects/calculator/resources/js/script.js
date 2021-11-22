17848fb21e3f1deb87513a08dec16e8ff81854a6
Jordantmp <jgroseclosej.i.g@gmail.com>
2021-11-22 13:08:48 -0500

    War L60

---
+++ projects/calculator/resources/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("num1").oneclick = function write1()
+    {
+      document.getElementById("screen").innerHTML = "1";
+    }
+
+    document.getElementById("clear").oneclick = function write1()
+        {
+          document.getElementById("clear").innerHTML = "";
+        }
